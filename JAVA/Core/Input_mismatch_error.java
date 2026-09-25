be81ccd5cc477006a3ede6d764fa49a27a606a89
Souvik Chakraborty <chakraborty.souvik8777@gmail.com>
2026-09-25 23:30:33 +0530

    Add InputMismatchError handling

---
+++ JAVA/Core/Input_mismatch_error.java
@@ -0,0 +1,21 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class Try1 {
+    public static void main(String as[]) {
+        try {
+            int a, b, c;
+            Scanner ob = new Scanner(System.in);
+            System.out.print("Enter the 1st No. = ");
+            a = ob.nextInt();
+            System.out.print("Enter the 2nd No. = ");
+            b = ob.nextInt();
+            c = a / b;
+            System.out.print("Division = " + c);
+        } catch (InputMismatchException e) {
+            System.out.println("\nInput Mismatch Exception !!");
+        } catch (Exception e) {
+            System.out.print("Arithmetic division problem / by 0 can't be possible \n" + e);
+        }
+    }
+}
